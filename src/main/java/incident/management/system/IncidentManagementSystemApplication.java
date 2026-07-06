5a22b4cf6c0e02c5053567b3759f8be952be84f3
ziyadsamhaoui <ziyad.samhaoui@usms.ac.ma>
2026-07-06 09:25:43 +0100

    intial commit

---
+++ src/main/java/incident/management/system/IncidentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package incident.management.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncidentManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/incident/management/system/IncidentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package incident.management.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
