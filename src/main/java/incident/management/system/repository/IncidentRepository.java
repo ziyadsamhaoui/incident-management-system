48d7b512f2c7276a5077d8980d9f63295e9c372d
ziyadsamhaoui <ziyad.samhaoui@usms.ac.ma>
2026-07-07 16:27:14 +0100

    feat: added request methods in the repository layer

--- src/main/java/incident/management/system/repository/IncidentRepository.java
+++ src/main/java/incident/management/system/repository/IncidentRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.Optiona;
 
 @Repository
 public interface IncidentRepository extends JpaRepository<IncidentEntity, Long> {
