397443cf724c187700b532e388e6808d527b4d1f
ziyadsamhaoui <ziyad.samhaoui@usms.ac.ma>
2026-07-07 16:27:51 +0100

    feat: created & implemented incident service

--- src/main/java/incident/management/system/repository/IncidentRepository.java
+++ src/main/java/incident/management/system/repository/IncidentRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optiona;
+import java.util.Optional;
 
 @Repository
 public interface IncidentRepository extends JpaRepository<IncidentEntity, Long> {
